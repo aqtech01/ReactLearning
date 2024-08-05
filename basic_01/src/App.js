ff45280cd5c83b50972a0db147e7c09b372a2d8d
aqtech <abdulqadeer0120@gmail.com>
2024-08-05 12:23:52 +0100

    start

---
+++ basic_01/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>This is Sparta</h1>;
+}
+
+export default App;
---
+++ basic_vite_01/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>AQTECH VITE</h1>
+    </>
+  );
+}
+
+export default App;
